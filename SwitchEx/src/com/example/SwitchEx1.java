e36e7a624331fa8aeae249e0d858e1c6e812210b
Jamin Brako <jamin.brako@gmail.com>
2020-04-02 12:39:04 -0400

    first commit

---
+++ SwitchEx/src/com/example/SwitchEx1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example;
+
+/**
+ *
+ * @author anshenoy
+ */
+public class SwitchEx1 {
+
+    public static void main(String args[]) {
+    }
+}
