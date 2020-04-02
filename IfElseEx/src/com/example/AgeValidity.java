771e9cf1e1f20c92eb22a9b88938325ca8598d8a
Jamin Brako <jamin.brako@gmail.com>
2020-04-02 13:12:25 -0400

    (finished) AgeValidity.java

--- IfElseEx/src/com/example/AgeValidity.java
+++ IfElseEx/src/com/example/AgeValidity.java
@@ -14,7 +14,17 @@ import java.util.Scanner;
 public class AgeValidity {
 
     public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    int age = myObj.nextInt();
 
-       
+    System.out.println("Enter age");
+    
+    
+       boolean drivingUnderAge;
+      drivingUnderAge = false;
+      int x = 18;
+      int y = x++;
+      drivingUnderAge = x <= y;
+       System.out.println(drivingUnderAge);
     }
 }
